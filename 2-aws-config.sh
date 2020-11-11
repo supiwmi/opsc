aa32db7c42309e42b99a66a67ce7205d702d1b0a
supiwmi <35697929+supiwmi@users.noreply.github.com>
2020-11-11 07:49:44 +0700

    Update 2-aws-config.sh

--- 2-aws-config.sh
+++ 2-aws-config.sh
@@ -1,1 +1,10 @@
+#!/bin/bash
+
+touch ~/.aws/credentials
+touch ~/.aws/config
+cat /root/helper/.sensitive/credentials  >  ~/.aws/credentials
+cat /root/helper/.sensitive/config  >  ~/.aws/config
+
+aws sts get-caller-identity
+aws ec2 describe-instances
 
