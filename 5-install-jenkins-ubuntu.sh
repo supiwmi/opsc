fc8eabec155975c98ef4d13abbcedc098c72d308
supiwmi <35697929+supiwmi@users.noreply.github.com>
2020-11-17 08:04:31 +0700

    Update 5-install-jenkins-ubuntu.sh

--- 5-install-jenkins-ubuntu.sh
+++ 5-install-jenkins-ubuntu.sh
@@ -10,19 +10,21 @@ JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
 JRE_HOME=/usr/lib/jvm/java-8-openjdk-amd64/jre
 EOL
 
+java -version
+
 ### Step 2 — Installing Jenkins
 
-#wget -q -O - http://pkg.jenkins-ci.org/debian/jenkins-ci.org.key | sudo apt-key add -
-#sudo sh -c 'echo deb http://pkg.jenkins-ci.org/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
+wget -q -O - http://pkg.jenkins-ci.org/debian/jenkins-ci.org.key | sudo apt-key add -
+sudo sh -c 'echo deb http://pkg.jenkins-ci.org/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
 
-#sudo apt install jenkins
+sudo apt install jenkins
 
 ### Step 3 — Starting Jenkins
 
-#sudo systemctl start jenkins
-#sudo systemctl enable jenkins
-#sudo systemctl status jenkins
+sudo systemctl start jenkins
+sudo systemctl enable jenkins
+sudo systemctl status jenkins
 
 ### Step 4 — Setting Up Jenkins
-#sudo cat /var/lib/jenkins/secrets/initialAdminPassword
-#echo "Navigate to Web Broswer http://your_server_ip_or_domain:8080"
+sudo cat /var/lib/jenkins/secrets/initialAdminPassword
+echo "Navigate to Web Broswer http://your_server_ip_or_domain:8080"
