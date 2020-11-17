c914ea022178b959d3ac3f1b1c2dc7c5fe79dd2b
supiwmi <35697929+supiwmi@users.noreply.github.com>
2020-11-17 07:56:43 +0700

    Create 5-install-jenkins-ubuntu.sh

---
+++ 5-install-jenkins-ubuntu.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+sudo apt update
+### Step 1 — Installing Java (prerequisite)
+
+sudo apt install openjdk-8-jdk openjdk-8-jre
+
+sudo cat >> /etc/environment <<EOL
+JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
+JRE_HOME=/usr/lib/jvm/java-8-openjdk-amd64/jre
+EOL
+
+### Step 2 — Installing Jenkins
+
+#wget -q -O - http://pkg.jenkins-ci.org/debian/jenkins-ci.org.key | sudo apt-key add -
+#sudo sh -c 'echo deb http://pkg.jenkins-ci.org/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
+
+#sudo apt install jenkins
+
+### Step 3 — Starting Jenkins
+
+#sudo systemctl start jenkins
+#sudo systemctl enable jenkins
+#sudo systemctl status jenkins
+
+### Step 4 — Setting Up Jenkins
+#sudo cat /var/lib/jenkins/secrets/initialAdminPassword
+#echo "navigate to webbroswer http://your_server_ip_or_domain:8080"
