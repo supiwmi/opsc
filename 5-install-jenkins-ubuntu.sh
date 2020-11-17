79503c29581e3038e8d7c5bb90375238b4e452d3
supiwmi <35697929+supiwmi@users.noreply.github.com>
2020-11-17 09:46:22 +0700

    Update 5-install-jenkins-ubuntu.sh

--- 5-install-jenkins-ubuntu.sh
+++ 5-install-jenkins-ubuntu.sh
@@ -14,11 +14,12 @@ java -version
 
 ### Step 2 — Installing Jenkins
 
-wget -q -O - http://pkg.jenkins-ci.org/debian/jenkins-ci.org.key | sudo apt-key add -
-sudo sh -c 'echo deb http://pkg.jenkins-ci.org/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
+wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
+sudo sh -c 'echo deb https://pkg.jenkins.io/debian-stable binary/ > \
+    /etc/apt/sources.list.d/jenkins.list'
+sudo apt-get update
+sudo apt-get install jenkins
 
-sudo apt update
-sudo apt install -y jenkins
 
 ### Step 3 — Starting Jenkins
 
